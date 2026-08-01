2f148858b02833daba240163fc8979e7c75b60f6
Andre Baltazar <me@andrebaltazar.com>
2026-08-01 22:42:25 +0100

    Pin Nix development environment

--- shell.nix
+++ shell.nix
@@ -1,4 +1,8 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (builtins.fetchTarball {
+    name = "nixpkgs-5e2a59a5b1a82f89f2c7e598302a9cacebb72a67";
+    url = "https://github.com/NixOS/nixpkgs/archive/5e2a59a5b1a82f89f2c7e598302a9cacebb72a67.tar.gz";
+    sha256 = "02rf59xj2v1ml7v35x4j64ax1fb7xx96ld7va6rifyxazmwsr4rb";
+  }) {} }:
 
 pkgs.mkShell {
   packages = with pkgs; [
